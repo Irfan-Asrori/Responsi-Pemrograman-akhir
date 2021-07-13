ce4571a81a884e8b5f9a38f4d471de80c4bd41a4
Irfan Asrori <irfanasrori@students.amikom.ac.id>
2021-07-13 15:20:44 +0700

    inisialisasi project

---
+++ Mahasiswa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        public string nim { get; set; }
+        public string nama { get; set; }
+        public string kelamin { get; set; }
+        public string ipk { get; set; }
+
+        public void tambah()
+        {
+            Console.Write("\nNIM: ");
+            Mahasiswa.nim = nim = Console.ReadLine();
+            Console.Write("\nNama: ");
+            Mahasiswa.nama = nama = Console.ReadLine();
+            Console.Write("\nJenis Kelamin : ");
+            Mahasiswa.kelamin = kelamin = Console.ReadLine();
+            Console.Write("\nIPK : ");
+            Mahasiswa.ipk = ipk = Console.ReadLine();
+
+            daftarMahasiswa.Add(Mahasiswa);
+        }
+
+        public void tampil()
+        {
+            Console.WriteLine("\nData Mahasiswa");
+            foreach (Mahasiswa mahasiswa in list)
+            {
+                Console.WriteLine("1. {0}, {1}, {2}, {3}, {4}, {5}", mahasiswa.nim, mahasiswa.nama, mahasiswa.kelamin, mahasiswa.ipk);
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
